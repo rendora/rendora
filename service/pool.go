9200784e9b60920557e893a89bf5bafa7914cef5
Paul <paul@links123.com>
2019-09-27 23:29:56 +0800

    add ping

--- service/pool.go
+++ service/pool.go
@@ -1,6 +1,8 @@
 package service
 
-import "github.com/silenceper/pool"
+import (
+	"github.com/silenceper/pool"
+)
 
 // NewHeadlessClientPool creates HeadlessClientPool
 func NewHeadlessClientPool(cfg *HeadlessConfig) (pool.Pool, error) {
@@ -17,11 +19,16 @@ func NewHeadlessClientPool(cfg *HeadlessConfig) (pool.Pool, error) {
 		return v.(*HeadlessClient).Close()
 	}
 
+	ping := func(v interface{}) error {
+		return v.(*HeadlessClient).Ctx.Err()
+	}
+
 	poolConfig := &pool.Config{
-		InitialCap: 15,
-		MaxCap:     30,
+		InitialCap: 10,
+		MaxCap:     20,
 		Factory:    factory,
 		Close:      close,
+		Ping:       ping,
 		//连接最大空闲时间，超过该时间的连接 将会关闭，可避免空闲时连接EOF，自动失效的问题
 		IdleTimeout: 0,
 	}
